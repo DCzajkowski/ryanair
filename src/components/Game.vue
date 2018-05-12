0b103e61c59fb527688afaf5c2f3cfb5e67490a5
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-12 23:02:32 +0200

    Phaser initial work

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-
+        <div id="canvas"></div>
     </div>
 </template>
 
 <script>
+    import Phaser from 'phaser'
+
     export default {
-        //
+        methods: {
+            preload() {
+                //
+            },
+            create() {
+                //
+            },
+        },
+        mounted() {
+            const game = new Phaser.Game({
+                parent: 'canvas',
+                type: Phaser.AUTO,
+                // width: window.innerWidth * window.devicePixelRatio,
+                // height: window.innerHeight * window.devicePixelRatio,
+                width: 1280,
+                height: 720,
+                backgroundColor: '#000000',
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        gravity: { y: 0 },
+                    },
+                },
+                scene: {
+                    preload: this.preload,
+                    create: this.create,
+                },
+            })
+        }
     }
 </script>
