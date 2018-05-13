db26c73003d97e18a112e080b21cf63e788bb3bf
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-13 13:58:59 +0200

    Zegarek

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img @click="$router.push({ name: 'Menu' })" v-if="a === 1" src="/static/card1.png" class="h-screen w-screen">
-        <img @click="$router.push({ name: 'Menu' })" v-else src="/static/card2.png" class="h-screen w-screen">
+        <img @click="$router.push({ name: 'Menu' })" v-else src="/static/zegarek.png" class="h-screen w-screen">
     </div>
 </template>
 
